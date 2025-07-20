8d2133c8397100955844570c5a43b14aeb6a99b5
unknown <you@example.com>
2025-07-20 16:32:22 +0530

    fixed bugs

--- controllers/visitor.controller.js
+++ controllers/visitor.controller.js
@@ -1,4 +1,5 @@
 import {  visitor } from "../model/visitor.model.js"
+import { sendMail } from "../Service/sendMail.js"
 
  const controllVisitor = async (req,res)=>{
     
@@ -32,6 +33,8 @@ import {  visitor } from "../model/visitor.model.js"
 
     const newVisitor = new visitor({fullname,email,quarries})
     newVisitor.save()
+
+     sendMail(email,"Thankyou for Checking out  My Portfolio",`Hii, ${fullname} I'll contact you soon ;-)`)
 
       return res.status(201).send({
         success: true,
